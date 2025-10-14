1130902eecdeb4693c8286a857917e6b4aa858dc
Arzubek1 <arzubek729@gmail.com>
2025-10-14 10:53:34 +0600

    lskdfj

---
+++ src/components/testiq/Page9.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React, { FC, useState } from "react";
+import scss from "./AllPages.module.scss";
+import { useAppDispatch } from "@/hooks";
+import { handleCorrect, handleScore } from "@/toolkit/testSlice";
+import { GrFormNextLink } from "react-icons/gr";
+
+const variants: string[] = [
+  "A) Книга",
+  "B) Тетрадь",
+  "C) Ручка",
+  "D) Бумага",
+];
+interface TestProps {
+  onNext?: () => void;
+}
+
+const Page9: FC<TestProps> = ({onNext}) => {
+      const [isDisabled, setIsDisabled] = useState(false);
+      const [answers, setAnswers] = useState(false);
+    
+      const dispatch = useAppDispatch();
+      const handleChoose = (choose: string) => {
+        setAnswers(true);
+        if (choose === "C) Ручка") {
+          dispatch(handleScore());
+        }
+        if (isDisabled) return;
+        setIsDisabled(true);
+        dispatch(handleCorrect(true));
+        setTimeout(() => {
+          dispatch(handleCorrect(false));
+        }, 700);
+      };
+    return (
+    <div className={scss.block}>
+      <div className={scss.text}>
+        <h4>Ассоциация</h4>
+        <p>
+          {!answers
+            ? "Вопрос: Какое слово лишнее: “Книга, Тетрадь, Ручка, Бумага”?"
+            : "✅ Правильный ответ: C. Ручка — инструмент, остальные — бумажные предметы."}
+        </p>
+      </div>
+      <div className={scss.varinats}>
+        {variants.map((el, idx) => (
+          <button
+            key={idx}
+            disabled={isDisabled}
+            onClick={() => handleChoose(el)}
+          >
+            {el}
+          </button>
+        ))}
+        <h5 onClick={() => onNext?.()}>
+          Следюший <GrFormNextLink />
+        </h5>
+      </div>
+    </div>
+    );
+};
+
+export default Page9;
